792fc82f62f16aa4f0c07d91d09638fa724e6631
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-08-08 16:38:34 +0900

    feat: color, blur, border to appearance.lua

--- arch/hypr/.config/hypr/lua/appearance.lua
+++ arch/hypr/.config/hypr/lua/appearance.lua
@@ -1,10 +1,19 @@
--- Appearance configuration
-
 hl.config({
     general = {
         gaps_in = 5,
         gaps_out = 10,
         border_size = 2,
+
+        col = {
+            active_border = {
+                colors = {
+                    "rgba(33ccffee)",
+                    "rgba(00ff99ee)",
+                },
+                angle = 45,
+            },
+            inactive_border = "rgba(595959aa)",
+        },
 
         resize_on_border = false,
         allow_tearing = false,
@@ -16,5 +25,22 @@ hl.config({
         rounding_power = 2,
         active_opacity = 1.0,
         inactive_opacity = 1.0,
+
+        shadow = {
+            enabled = true,
+            range = 4,
+            render_power = 3,
+            color = "rgba(1a1a1aee)",
+        },
+
+        blur = {
+            enabled = true,
+            size = 10,
+            passes = 3,
+            vibrancy = 0.1696,
+            noise = 0.02,
+            contrast = 1.1,
+            brightness = 0.9,
+        },
     },
 })
