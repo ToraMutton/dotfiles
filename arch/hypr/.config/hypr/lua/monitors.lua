9bbacdb1e4d745ed62ce129d6d490140118f2220
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-08-08 15:55:38 +0900

    feat: monitors.lua

---
+++ arch/hypr/.config/hypr/lua/monitors.lua
@@ -0,0 +1,16 @@
+-- Monitor configuration
+
+hl.monitor({
+    output = "HDMI-A-1",
+    mode = "2560x1440@75",
+    position = "0x0",
+    scale = 1,
+    transform = 1,
+})
+
+hl.monitor({
+    output = "DP-2",
+    mode = "2560x1440@180",
+    position = "1440x560",
+    scale = 1,
+})
