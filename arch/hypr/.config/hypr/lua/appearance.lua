92acc95e2704befe771ebfdbf3f0c80a42f15116
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-08-08 16:27:44 +0900

    feat: appearance.lua

---
+++ arch/hypr/.config/hypr/lua/appearance.lua
@@ -0,0 +1,20 @@
+-- Appearance configuration
+
+hl.config({
+    general = {
+        gaps_in = 5,
+        gaps_out = 10,
+        border_size = 2,
+
+        resize_on_border = false,
+        allow_tearing = false,
+        layout = "dwindle",
+    },
+
+    decoration = {
+        rounding = 10,
+        rounding_power = 2,
+        active_opacity = 1.0,
+        inactive_opacity = 1.0,
+    },
+})
