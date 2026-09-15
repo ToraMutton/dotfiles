09b3f7bae786393c2ac0b9311ef19f283c5e2e5d
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-09-15 17:34:51 +0900

    chore(cliphist): limit clipboard history

--- arch/hypr/.config/hypr/lua/autostart.lua
+++ arch/hypr/.config/hypr/lua/autostart.lua
@@ -32,6 +32,6 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("~/scripts/discord-ipc-link.sh")
 
     -- Clipboard history
-    hl.exec_cmd("wl-paste --type text --watch cliphist store")
-    hl.exec_cmd("wl-paste --type image --watch cliphist store")
+    hl.exec_cmd("wl-paste --type text --watch cliphist -max-items 20 store")
+    hl.exec_cmd("wl-paste --type image --watch cliphist -max-items 20 store")
 end)
